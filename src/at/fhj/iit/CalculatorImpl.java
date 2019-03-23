b2b0a6f696bf4968d09350e4254676016b6c721d
Henrik Funke <henrik.funke@edu.campus02.at>
2019-03-23 10:12:03 +0100

    created implementation class for Calculator + changed addValue(int Value) accordingly; using an ArrayList for all current saved integers

---
+++ src/at/fhj/iit/CalculatorImpl.java
@@ -0,0 +1,19 @@
+package at.fhj.iit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculatorImpl implements Calculator {
+  private List<Integer> values = new ArrayList<Integer>();
+  @Override
+    public int getMaximum() {
+      return 0;
+    }
+  @Override
+    public int getMinimum() {
+      return 0;
+    }
+  @Override
+    public void addValue(int value) {
+    }
+  
+}
